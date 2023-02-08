b3627dd8f232433de31fa73eced5577cb906e79c
prosyae2 <evelina.prosyankina2@mail.dcu.ie>
2023-02-08 21:34:27 +0000

    Minor edits

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,23 +1,18 @@
 from fastapi import FastAPI
 from random import choice
 
-
 busylabs_backend = FastAPI()
-
 
 # read_root(data_from_app)
 # location, time, ssid, bssid, rssi, ap list
 
-
-# adjust?
 @busylabs_backend.get("/")
 def read_app_data(location: str, time: int, aplist: str):
     aplist = calculate_prediction(aplist.strip("{}").split())
     return {"result": aplist}
 
 
-
-
+# emulation of prediction -- not yet integrated
 def calculate_prediction(aplist):
     print("\n\npredicted!\n\n")
     p = ["lg25", "lg26", "lg27"]
