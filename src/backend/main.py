9f43a026647538c82334eed506dceffd93609935
Ethan <ethan.clarke42@mail.dcu.ie>
2023-02-21 17:22:00 +0000

    Added configurable IP and Port through command line arguments

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,17 +1,41 @@
-from fastapi import FastAPI
 from random import choice
 
-busylabs_backend = FastAPI()
+import uvicorn
+import sys
+from fastapi import FastAPI, HTTPException
+
+ip_address = sys.argv[1]
+port = int(sys.argv[2])
+
+description = """
+# BusyLabs - Backend API
+Returns McNulty lab location predictions 
+"""
+
+app = FastAPI(
+    title="BusyLabs API",
+    description=description
+)
 
 
-@busylabs_backend.get("/")
-def read_app_data(location: str, time: int, aplist: str):
-    aplist = emulate_prediction(aplist.strip("{}").split())
-    return {"result": aplist}
+@app.get('/room')
+async def read_app_data(ap_str: str):
+    ap_list = ap_str.split("|")
+
+    if len(ap_list) == 0 or ap_str is None:
+        raise HTTPException(status_code=406, detail="No Access Points provided")
+
+    result = emulate_prediction(ap_list)
+    return {"result": result}
 
 
 # emulation of prediction -- to be further developed
-def emulate_prediction(aplist):
+# TODO: Move this to its own file
+def emulate_prediction(ap_list):
     p = ["LG25", "LG26", "LG27", "L114", "L101"]
 
     return choice(p)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host=ip_address, port=port)
