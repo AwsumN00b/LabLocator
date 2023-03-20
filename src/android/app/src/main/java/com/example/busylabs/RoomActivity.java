f78c9894940916997b87c4cf4355fe48267c34bb
Ethan <ethan.clarke42@mail.dcu.ie>
2023-03-20 22:12:03 +0000

    Back button on RoomActivity works

--- src/android/app/src/main/java/com/example/busylabs/RoomActivity.java
+++ src/android/app/src/main/java/com/example/busylabs/RoomActivity.java
@@ -1,11 +1,12 @@
 package com.example.busylabs;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class RoomActivity extends AppCompatActivity {
+public class RoomActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView roomNameTextView;
 
@@ -16,5 +17,10 @@ public class RoomActivity extends AppCompatActivity {
         String roomName = getIntent().getExtras().getString("roomName");
         roomNameTextView = findViewById(R.id.textView_room_name);
         roomNameTextView.setText(roomName);
+    }
+
+    @Override
+    public void onClick(View view) {
+        onBackPressed();
     }
 }
