2ef7378d96c9f6413ad83dac2b060c177ba046c7
Ethan Clarke <ethan.clarke42@mail.dcu.ie>
2023-05-01 21:01:14 +0100

    Updated broken SQL queries

--- src/backend/main.py
+++ src/backend/main.py
@@ -102,15 +102,14 @@ def log_user_location(room, device_id, time):
     db_cursor.execute(sql, val)
 
     db_connection.commit()
-    print(f"{time}: User {device_id} has been logged in {room}")
 
 
 def get_device_locations_in_room_this_hour(room):
     sql = """
-    SELECT MIN(id), room, MAX(`time`)
+    SELECT id, room
 FROM lablocator_db.user_location_table
 WHERE `time` >= DATE_SUB(NOW(), INTERVAL 1 HOUR) AND room = %s
-GROUP BY room
+;
     """
     db_cursor.execute(sql, [room])
 
@@ -131,7 +130,7 @@ def room_population(query_list, room=None):
 
 def get_all_devices_this_hour():
     sql = """
-    SELECT MIN(id), room, MAX(`time`)
+    SELECT MIN(id), room
 FROM lablocator_db.user_location_table
 WHERE `time` >= DATE_SUB(NOW(), INTERVAL 1 HOUR)
 GROUP BY room
@@ -143,8 +142,7 @@ GROUP BY room
 
 
 def least_populated_room(room_data):
-    room_count = room_population(room_data)
-    return min(room_count, key=room_count.get)
+    return min(room_data, key=lambda t: t[0])[1]
 
 
 def zero():
